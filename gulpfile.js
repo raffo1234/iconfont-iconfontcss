a827e3e9718dfc3f6a44b5e138be434fde2e54c7
Rafael Meza <rhmcord@gmail.com>
2016-12-06 10:41:14 -0500

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,35 @@ var gulp = require('gulp')
 var iconfont = require('gulp-iconfont')
 var iconfontcss = require('gulp-iconfont-css')
 
+// gulp.task('iconfont', function(){
+//   return gulp.src(['assets/icons/*.svg'])
+//     .pipe(iconfontcss({
+//       fontName: 'icons',
+//       targetPath: '../../assets/css/icons.scss',
+//       fontPath: './'
+//     }))
+//     .pipe(iconfont({
+//       fontName: 'icons',
+//       formats: ['ttf', 'eot', 'woff'],
+//       normalize: true
+//     }))
+//     .pipe(gulp.dest('assets/css/'))
+// })
+
+// run gulp iconfont
 gulp.task('iconfont', function(){
   return gulp.src(['assets/icons/*.svg'])
     .pipe(iconfontcss({
       fontName: 'icons',
-      targetPath: '../../assets/css/icons.scss',
-      fontPath: './'
+      targetPath: '../../assets/css/icons.css',
+      fontPath: ''
     }))
     .pipe(iconfont({
-      fontName: 'icons',
-      formats: ['ttf', 'eot', 'woff'],
-      normalize: true
-    }))
-    .pipe(gulp.dest('assets/css/'))
-})
+  		fontName: 'icons',
+  		fontHeight: 1000,
+  		normalize: true
+      ,
+  		centerHorizontally: true
+  	}))
+    .pipe(gulp.dest('assets/css/'));
+});
